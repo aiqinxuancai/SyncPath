4633acd7ad92a28122b1120ed264573798f846af
aiqinxuancai <aiqinxuancai@163.com>
2023-07-26 11:50:28 +0800

    删除无用的代码

--- SyncPath/Program.cs
+++ SyncPath/Program.cs
@@ -54,13 +54,11 @@ namespace SyncPath // Note: actual namespace depends on the project name.
             DirectoryInfo sourceDirectory = new DirectoryInfo(sourcePath);
             DirectoryInfo targetDirectory = new DirectoryInfo(targetPath);
 
-            // Create the target directory if it doesn't exist
             if (!targetDirectory.Exists)
             {
                 targetDirectory.Create();
             }
 
-            // Sync files from source to target directory
             foreach (FileInfo sourceFile in sourceDirectory.GetFiles())
             {
                 string targetFilePath = Path.Combine(targetPath, sourceFile.Name);
@@ -70,9 +68,6 @@ namespace SyncPath // Note: actual namespace depends on the project name.
                 if (!targetFile.Exists || sourceFile.LastWriteTime > targetFile.LastWriteTime || sourceFile.Length != targetFile.Length)
                 {
                     Console.WriteLine("同步文件: " + sourceFile.Name);
-
-                    // Copy the file from source to target
-
                     try
                     {
                         sourceFile.CopyTo(targetFilePath, true);
@@ -80,14 +75,10 @@ namespace SyncPath // Note: actual namespace depends on the project name.
                     catch (Exception ex)
                     {
                         Console.WriteLine("同步文件错误: " + targetFile.Name);
-
                     }
-
-
                 }
             }
 
-            // Delete files from target directory that don't exist in source directory
             foreach (FileInfo targetFile in targetDirectory.GetFiles())
             {
                 string sourceFilePath = Path.Combine(sourcePath, targetFile.Name);
@@ -95,8 +86,6 @@ namespace SyncPath // Note: actual namespace depends on the project name.
                 if (!File.Exists(sourceFilePath))
                 {
                     Console.WriteLine("删除文件: " + targetFile.Name);
-
-                    // Delete the file from target
                     try
                     {
                         targetFile.Delete();
@@ -105,11 +94,9 @@ namespace SyncPath // Note: actual namespace depends on the project name.
                     {
                         Console.WriteLine("删除文件错误: " + targetFile.Name);
                     }
-
                 }
             }
 
-            // Sync directories recursively
             foreach (DirectoryInfo sourceSubDirectory in sourceDirectory.GetDirectories())
             {
                 string targetSubDirectoryPath = Path.Combine(targetPath, sourceSubDirectory.Name);
@@ -117,7 +104,6 @@ namespace SyncPath // Note: actual namespace depends on the project name.
                 SyncDirectories(sourceSubDirectory.FullName, targetSubDirectoryPath);
             }
 
-            // Delete subdirectories from target directory that don't exist in source directory
             foreach (DirectoryInfo targetSubDirectory in targetDirectory.GetDirectories())
             {
                 string sourceSubDirectoryPath = Path.Combine(sourcePath, targetSubDirectory.Name);
@@ -126,7 +112,6 @@ namespace SyncPath // Note: actual namespace depends on the project name.
                 {
                     Console.WriteLine("删除目录: " + targetSubDirectory.Name);
 
-                    // Delete the directory from target
                     try
                     {
                         targetSubDirectory.Delete(true);
@@ -139,7 +124,5 @@ namespace SyncPath // Note: actual namespace depends on the project name.
                 }
             }
         }
-
-
     }
 }
